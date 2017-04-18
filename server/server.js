c10b05fcf7a57d396a7ce4e375894113c3ef7bb9
tanhimawan14 <himawan.tan.b@gmail.com>
2017-04-18 13:24:13 +0700

    Basic app structure

---
+++ server/server.js
@@ -0,0 +1,14 @@
+// require('./config/config.js');
+
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+	console.log(`Started up at port ${port}`);
+});
